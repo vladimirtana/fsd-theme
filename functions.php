3a8461659c10503bb1c1766bcfbed97ee389da63
vladimirtana <vladimirtana96@gmail.com>
2020-07-12 13:33:06 +0200

    setting up default parts of theme

--- functions.php
+++ functions.php
@@ -5,6 +5,11 @@ function_fsd_theme_enqueue_styles()  {
  $dependencies = array('bootstrap');
  wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies );
 }
+
+function_fsd_theme_enqueue_scripts(){
+	{
+    $dependencies = array('jquery');
+wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '4.5.0', true );
 }
 
 
