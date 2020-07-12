68ebe475dc7d0b874e192e4bb4f2637e15910035
vladimirtana <vladimirtana96@gmail.com>
2020-07-12 13:18:19 +0200

    Integrating boostrap

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function_fsd_theme_enqueue_styles()  {
+ wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+ $dependencies = array('bootstrap');
+ wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies );
+}
+}
 
 
 ?>
